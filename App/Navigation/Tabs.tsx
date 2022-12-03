dc0697b27865e6caab327d999d74d87595cc9172
berkaygurcan <berkaygurcan141@gmail.com>
2022-12-03 17:43:40 +0300

    TabBar Style Added

--- App/Navigation/Tabs.tsx
+++ App/Navigation/Tabs.tsx
@@ -1,14 +1,50 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet } from 'react-native';
 import Home from '../Pages/Home';
+import { Colors } from '../Themes/Colors';
+
 
 const Tab = createBottomTabNavigator();
 
 function Tabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={
+      {
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: Colors.tabBar.barActiveTintColor,
+        tabBarInactiveTintColor: Colors.tabBar.barInactiveTintColor,
+        tabBarStyle: [
+          styles.tabBarStyle
+        ],
+        tabBarIconStyle: [
+          styles.tabBarIconStyle
+        ],
+      }
+    }>
       <Tab.Screen name="Home" component={Home} />
     </Tab.Navigator>
   );
 }
+
+
+const styles = StyleSheet.create({
+
+  tabBarStyle: {
+   bottom: 30,
+   right:35,
+   left:35,
+   height:"8%",
+   elevation:0,
+   borderRadius:10,
+   position:'absolute',
+   opacity:0.70,
+   backgroundColor:Colors.common.cardBgColor,
+  },
+  tabBarIconStyle: {
+    top:10,
+  }
+});
+
 
 export default Tabs;
--- App/Themes/Colors.tsx
+++ App/Themes/Colors.tsx
@@ -6,6 +6,11 @@ const Colors = {
         grey: "#777777",
         white: "#fff"
     },
+    tabBar: {
+        bgColor: "#3B3B3B",
+        barActiveTintColor:"#9747FF",
+        barInactiveTintColor:"#fff",
+    },
     metascore: {
         good: "#6dc849",
         normal: "#fdca52",
